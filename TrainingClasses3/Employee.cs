13ad492ee290d4e46b0a696b8ee144b5fece71b2
Дубинин Алексей <njn6589@mail.ru>
2023-07-09 14:47:20 +0300

    Фиксирую изменения в созданной ветке

---
+++ TrainingClasses3/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingClasses3
+{
+    /// <summary>
+    /// Объявление класса Сотрудник
+    /// </summary>
+    internal class Employee
+    {
+            /// <summary>
+            /// Объявление поле класса. Имя, возраст, должность
+            /// </summary>
+            private string _name;
+            private int _age;
+            private string _position;
+            /// <summary>
+            /// Объявление аксессера зарплата, так как его значение понадобится нам в Program для подсчета суммы зарплат всех сотрудников
+            /// </summary>         
+            public int _salary { get; set; }
+
+            /// <summary>
+            /// Объявление конструктора с параметрами
+            /// </summary>
+            public Employee(string name, int age, string position, int salary)
+            {
+                this._name = name;
+                this._age = age;
+                this._position = position;
+                this._salary = salary;
+            }
+            /// <summary>
+            /// Объявление конструктора без параметров
+            /// </summary>
+            public Employee() 
+            { 
+            }
+            /// <summary>
+            /// Объявление метода ТоString, для преобразования объекта в строковое представление. Для красивого вывода объявлена переменная info
+            /// </summary>
+            /// <returns> переопределённая строка info с информацией о сотруднике</returns>
+            public override string ToString()
+            {
+                var info = $"Имя сотрудника: {_name} {Environment.NewLine}Возраст сотрудника: {_age} {Environment.NewLine}Позиция сотрудника: {_position} {Environment.NewLine}Зарплата сотрудника: {_salary}";
+                return info;
+            }
+    }
+}
